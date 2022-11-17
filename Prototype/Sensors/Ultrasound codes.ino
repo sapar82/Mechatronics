57febaf2989b7c1d96fa07d3a02dc9b5d6649e5e
bgatesson <77785536+bgatesson@users.noreply.github.com>
2022-11-17 18:02:00 +0000

    Add files via upload

---
+++ Prototype/Sensors/Ultrasound codes.ino
@@ -0,0 +1,44 @@
+//www.elegoo.com
+//2016.12.08
+#include "SR04.h"
+#define TRIG_PIN 10
+#define ECHO_PIN 9
+
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+
+int dist;
+const int threshold = 1000;
+int counter = 0;
+
+void setup() {
+   Serial.begin(9600);
+   delay(500);
+}
+
+void loop() {
+  dist=sr04.Distance();
+  bool state = false;
+  if (dist < thresold){
+    if (state == false){
+      counter++;
+      state = true;
+    }
+  }
+  else{
+    state = false;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
