a2b46141b2eb241761d0b9998183348d0eb8633e
bgatesson <77785536+bgatesson@users.noreply.github.com>
2023-01-23 19:40:17 +0000

    Update main.cpp

--- Prototype/src/main.cpp
+++ Prototype/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <string.h>
 
-// Temperature and humidity sensor
+// Temperature and humidty sensor
 #include "DHT.h"
 #define DHTPIN 11
 #define DHTTYPE DHT11
@@ -11,21 +11,20 @@ float humidity;
 
 // Ultrasonic sensor
 #include "SR04.h"
-int TRIG_PIN = 5;
+int TRIG_PIN = 5; // assign pin 5 and 6 to first ultrasonic sensor
 int ECHO_PIN = 6;
-SR04 dist_sensor_in = SR04(ECHO_PIN,TRIG_PIN);
+SR04 dist_sensor_in = SR04(ECHO_PIN,TRIG_PIN); // first sensor
 
-
-int TRIG_PIN_2 = 3;
+int TRIG_PIN_2 = 3; // assign pin 3 and 4 to second ultrasonic sensor
 int ECHO_PIN_2 = 4;
-SR04 dist_sensor_out = SR04(ECHO_PIN_2,TRIG_PIN_2);
-
-int distance_in;
-int distance_out;
-int counter = 0;
+SR04 dist_sensor_out = SR04(ECHO_PIN_2,TRIG_PIN_2); // second sensor
+// initialize variables for sensors 
+int distance_in; // distance measured by sensor 1
+int distance_out; // distance measured by sensor 2
+int counter = 0; // number of people inside 
 bool previous_state_in = false;
 bool previous_state_out = false;
-const int distance_treshold = 30;
+const int distance_treshold = 30; // decision threshold 
 
 // Display
 #include <epd2in9_V2.h>
@@ -72,16 +71,16 @@ void initialization_sd(){
   Serial.println("initialization done.");
 
   if (SD.exists("data.txt")) { // if the file exists, append data to it: 
-  //TODO change this so we can hardcode the filename as a constant
     Serial.println("data.txt exists. Appending data.");
   } else {
     Serial.println("data.txt doesn't exist. Creating a new file.");
-    myFile = SD.open("data.txt", FILE_WRITE);
+    myFile = SD.open("data.txt", FILE_WRITE); // else create a file if none detected
     myFile.println("Date, Time, Temperature, Humidity, Number of people");
     myFile.close();
   }
 }
 
+// function to write data to SD card
 void write_data( String date, String time, float temperature, float humidity, int counter){
   myFile = SD.open("data.txt", FILE_WRITE);
   if (myFile) {
@@ -92,6 +91,7 @@ void write_data( String date, String time, float temperature, float humidity, in
   }
 }
 
+// function to initialize e-paper screen
 void init_screen()
 {Serial.begin(9600);
   if (epd.Init() != 0) {
@@ -112,8 +112,9 @@ void init_screen()
   current_time = millis();      
 }
 
+// function to update the screen with new measurements every 5 min
 void loop_screen(String time, String counter, String temperature, String humidity) {
-  // put your main code here, to run repeatedly:
+  // Update for time
   char time_string[] = {'C','u','r','r','e','n','t',' ','t','i','m','e',':', '0', '0', ':', '0', '0','\0'};
   time_string[13] = time[0];
   time_string[14] = time[1];
@@ -126,7 +127,7 @@ void loop_screen(String time, String counter, String temperature, String humidit
   paint.Clear(UNCOLORED);
   paint.DrawStringAt(0, 4, time_string, &Font16, COLORED);
   epd.SetFrameMemory_Partial(paint.GetImage(), 100, 00, paint.GetWidth(), paint.GetHeight());
-  
+  // update for temperature
   char temp_string[] = {'T', 'e', 'm', 'p', 'e', 'r', 'a', 't', 'u', 'r', 'e', ':', ' ', '-', '-', '.', '-', 'F', '\0'};
 
   temp_string[13] = temperature[0];
@@ -140,7 +141,7 @@ void loop_screen(String time, String counter, String temperature, String humidit
   paint.Clear(UNCOLORED);
   paint.DrawStringAt(0, 4, temp_string, &Font16, COLORED);
   epd.SetFrameMemory_Partial(paint.GetImage(), 70, 0, paint.GetWidth(), paint.GetHeight());
-
+  // update for humidity
   char humidity_string[] = {'H', 'u', 'm', 'i', 'd', 'i', 't', 'y', ':', ' ', '-', '-', '.','-','-', '%', '\0'};
   humidity_string[10] = humidity[0];
   humidity_string[11] = humidity[1];
@@ -156,7 +157,7 @@ void loop_screen(String time, String counter, String temperature, String humidit
   paint.Clear(UNCOLORED);
   paint.DrawStringAt(0, 4, humidity_string, &Font16, COLORED);
   epd.SetFrameMemory_Partial(paint.GetImage(), 40, 0, paint.GetWidth(), paint.GetHeight());
-
+  // update for person counter 
   char counter_string[] = {'P', 'e', 'o', 'p', 'l', 'e', ' ', 'i', 'n', 's', 'i', 'd', 'e', ':', ' ', '0', '0', '0', '0', '\0'};
 
   counter_string[15] = counter[0];
@@ -172,30 +173,29 @@ void loop_screen(String time, String counter, String temperature, String humidit
   paint.DrawStringAt(0, 4, counter_string, &Font16, COLORED);
   epd.SetFrameMemory_Partial(paint.GetImage(), 10, 0, paint.GetWidth(), paint.GetHeight());
   epd.DisplayFrame_Partial();
-
-  // delay(300);
 }
 
+// function of the sensors that update the counter when persons go in and out of detected area
 void call_sensor(){
-    distance_in = dist_sensor_in.Distance();
+    distance_in = dist_sensor_in.Distance(); // Detect when a person enters
   if (distance_in < distance_treshold){
   if (!previous_state_in){
-    counter++;
-    previous_state_in = true;
+    counter++; // only adds 1 if person just entered the field
+    previous_state_in = true; 
     }
   }
   else{
     previous_state_in = false;
   }
 
-  Serial.print("Distance sensor in: ");
+  Serial.print("Distance sensor in: "); // for debugging and knowing the rate of detection of sensor
   Serial.print(distance_in);
   Serial.print("\n");
 
   distance_out = dist_sensor_out.Distance(); // Detect when a person leaves
   if (distance_out < distance_treshold){
     if (!previous_state_out){
-      counter--;
+      counter--; // only substract 1 if person just entered the field
       previous_state_out = true;
     }
   }
@@ -203,11 +203,11 @@ void call_sensor(){
     previous_state_out = false;
   }
 
-  Serial.print("Distance sensor out: ");
+  Serial.print("Distance sensor out: "); // for debugging and knowing the rate of detection of sensor
   Serial.print(distance_out);
   Serial.print("\n");
 }
-
+// setup code goes here 
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting...");
@@ -222,12 +222,13 @@ void setup() {
   Serial.print("Setup completed");
 }
 
+// loop code goes here 
 void loop() {
   
-  call_sensor();
+  call_sensor(); // ultrasound sensors measurements
 
-  dt = clock.getDateTime();
-  if ( dt.second % 315 == 0 )
+  dt = clock.getDateTime(); // get data from clock module
+  if ( dt.second % 315 == 0 ) // every 5 min 
   {
     Serial.println("Reading sensor");
     // Get time
@@ -245,7 +246,8 @@ void loop() {
     Serial.println("Data written to file:");
     Serial.println(date + String(",") + time + String(",") + String(temperature) + String(",") + String(humidity) + String(",") + String(counter));
   }
+  // string for person counter
   String counter_string = String(counter/1000) + String(counter/100 % 10) + String(counter/10 % 10) + String(counter % 10);
-    loop_screen(String(time), String(counter), String(temperature), String(humidity));
-  delay(1000);
+    loop_screen(String(time), String(counter), String(temperature), String(humidity)); // update screen with current values
+  delay(1000); // idk if we need this
 }
